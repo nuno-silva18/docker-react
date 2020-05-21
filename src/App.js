899afcadea78360caf26efbcfe17d1d619180039
Nuno Gonçalo Silva <nunogoncalo.silva@farfetch.com>
2020-05-21 15:27:02 +0100

    [FEAT] Added some cool new text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-	  Hello, friend...
+	  Hello, it is I, Feature branch lad!
         </p>
         <a
           className="App-link"
